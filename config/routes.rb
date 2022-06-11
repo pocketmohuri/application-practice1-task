d2d7cdaf3f7111a7428f6255e4c6acdbae442125
EC2 Default User <ec2-user@ip-172-31-10-57.ap-northeast-1.compute.internal>
2022-06-11 03:24:31 +0000

    [Finish 3-12]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'bookers/new'
+  get 'bookers/index'
+  get 'bookers/show'
+  get 'bookers/edit'
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
